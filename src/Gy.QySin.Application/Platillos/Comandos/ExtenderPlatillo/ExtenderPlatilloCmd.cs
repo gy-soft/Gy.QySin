5e0ac11b12e237590395ec7fffbd1fd01178b04e
Guayaba Software <guayaba.software@gmail.com>
2021-11-19 14:54:33 -0600

    comando extender platillo

--- src/Gy.QySin.Application/Platillos/Comandos/ExtenderPlatillo/ExtenderPlatilloCmd.cs
+++ src/Gy.QySin.Application/Platillos/Comandos/ExtenderPlatillo/ExtenderPlatilloCmd.cs
@@ -4,39 +4,39 @@ using Gy.QySin.Application.Common;
 using Gy.QySin.Domain.Entities;
 using MediatR;
 
-namespace Gy.QySin.Application.Bebidas.Comandos.ExtenderBebida
+namespace Gy.QySin.Application.Platillos.ExtenderPlatillo
 {
-    public class ExtenderBebidaCmd : IRequest
+    public class ExtenderPlatilloCmd : IRequest
     {
         public string Clave { get; set; }
         public string Nombre { get; set; }
         public string Imagen { get; set; }
-        public int? Contenido { get; set; }
-        public bool? Rellenable { get; set; }
+        public string Descripción { get; set; }
+        public bool? Vegetariano { get; set; }
     }
-    public class ExtenderBebidaCmdMnjr : IRequestHandler<ExtenderBebidaCmd>
+    public class ExtenderPlatilloCmdMnjr : IRequestHandler<ExtenderPlatilloCmd>
     {
         private readonly IApplicationDbContext context;
 
-        public ExtenderBebidaCmdMnjr(IApplicationDbContext context)
+        public ExtenderPlatilloCmdMnjr(IApplicationDbContext context)
         {
             this.context = context;
         }
-        public async Task<Unit> Handle(ExtenderBebidaCmd request, CancellationToken cancellationToken)
+        public async Task<Unit> Handle(ExtenderPlatilloCmd request, CancellationToken cancellationToken)
         {
             var pk = System.Guid.Parse(request.Clave);
-            var entity = await context.Bebidas
+            var entity = await context.Platillos
                 .FindAsync(pk);
 
             if (entity == null)
             {
-                throw new NotFoundException(nameof(Bebida), request.Clave);
+                throw new NotFoundException(nameof(Platillo), request.Clave);
             }
 
             entity.Nombre = request.Nombre ?? entity.Nombre;
             entity.Imagen = request.Imagen ?? entity.Imagen;
-            entity.Contenido = request.Contenido ?? entity.Contenido;
-            entity.Rellenable = request.Rellenable ?? entity.Rellenable;
+            entity.Descripción = request.Descripción ?? entity.Descripción;
+            entity.Vegetariano = request.Vegetariano ?? entity.Vegetariano;
 
             await context.SaveChangesAsync(cancellationToken);
             return Unit.Value;
