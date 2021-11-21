95a16f92ed49ac54b40109b9a71317a2937064de
Guayaba Software <guayaba.software@gmail.com>
2021-11-20 20:48:19 -0600

    poblar tabla platillos

--- sql/seed.sql
+++ sql/seed.sql
@@ -6,23 +6,32 @@ INSERT INTO "Usuarios"
 ("Clave", "NombreCorto", "Nombre", "Activo", "Roles")
 VALUES('6bc3649e-196b-4920-a14b-aff290aec9f0','Cloe','Valentina Uresti',true,'{"administrador"}');
 
-INSERT INTO "Ordenables"
-("Clave", "Nombre", "Precio", "Categoria")
+INSERT INTO "Ordenables" ("Clave", "Nombre", "Precio", "Categoria")
 VALUES('4d85b2d3-1abe-46cd-95f0-60846dbe4b78', 'Naranjada', 45, 'bebidas');
-INSERT INTO "Bebidas"
-("Clave", "Contenido", "Rellenable")
+INSERT INTO "Bebidas" ("Clave", "Contenido", "Rellenable")
 VALUES('4d85b2d3-1abe-46cd-95f0-60846dbe4b78', 450, true);
 
-INSERT INTO "Ordenables"
-("Clave", "Nombre", "Precio", "Categoria")
+INSERT INTO "Ordenables" ("Clave", "Nombre", "Precio", "Categoria")
 VALUES('6c6287bf-055c-4d26-a533-c7d78ef866f3', 'Limonada', 45, 'bebidas');
-INSERT INTO "Bebidas"
-("Clave", "Contenido", "Rellenable")
+INSERT INTO "Bebidas" ("Clave", "Contenido", "Rellenable")
 VALUES('6c6287bf-055c-4d26-a533-c7d78ef866f3', 450, true);
 
-INSERT INTO "Ordenables"
-("Clave", "Nombre", "Precio", "Categoria")
+INSERT INTO "Ordenables" ("Clave", "Nombre", "Precio", "Categoria")
 VALUES('972b9231-202e-426e-8c2d-3fbd539c78f5', 'Jugo de naranja', 55, 'bebidas');
-INSERT INTO "Bebidas"
-("Clave", "Contenido", "Rellenable")
-VALUES('972b9231-202e-426e-8c2d-3fbd539c78f5', 330, true);
+INSERT INTO "Bebidas" ("Clave", "Contenido", "Rellenable")
+VALUES('972b9231-202e-426e-8c2d-3fbd539c78f5', 330, false);
+
+INSERT INTO "Ordenables" ("Clave", "Nombre", "Precio", "Categoria")
+VALUES('a85f763e-4a74-11ec-957d-af9a5847120e', 'Milanesa de res', 120, 'platillos');
+INSERT INTO "Platillos" ("Clave", "Vegetariano")
+VALUES('a85f763e-4a74-11ec-957d-af9a5847120e', false);
+
+INSERT INTO "Ordenables" ("Clave", "Nombre", "Precio", "Categoria")
+VALUES('23c2a080-4a75-11ec-be6c-9f2f00b944c5', 'Milanesa de pollo', 120, 'platillos');
+INSERT INTO "Platillos" ("Clave", "Vegetariano")
+VALUES('23c2a080-4a75-11ec-be6c-9f2f00b944c5', false);
+
+INSERT INTO "Ordenables" ("Clave", "Nombre", "Precio", "Categoria")
+VALUES('53c87192-4a75-11ec-a2eb-df1c165275d1', 'Chile relleno de queso', 135, 'platillos');
+INSERT INTO "Platillos" ("Clave", "Vegetariano")
+VALUES('53c87192-4a75-11ec-a2eb-df1c165275d1', true);
