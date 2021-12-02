5d73c8154e6b0eda26661dcfeec771a84518dab8
Guayaba Software <guayaba.software@gmail.com>
2021-12-02 16:31:52 -0600

    vista precioordenables ui

--- sql/cleanup.sql
+++ sql/cleanup.sql
@@ -1,15 +1,18 @@
--- drop functions
-DROP FUNCTION IF EXISTS insert_into_platillos;
-DROP FUNCTION IF EXISTS update_platillos;
-DROP FUNCTION IF EXISTS insert_into_bebidas;
-DROP FUNCTION IF EXISTS update_bebidas;
-
--- drop tables
+-- drop tables and views
+DROP VIEW IF EXISTS "PrecioOrdenablesUi";
+DROP VIEW IF EXISTS "Bebidas";
+DROP VIEW IF EXISTS "Platillos";
 DROP TABLE IF EXISTS "PrecioOrdenables";
 DROP TABLE IF EXISTS "DetalleBebidas";
 DROP TABLE IF EXISTS "DetallePlatillos";
 DROP TABLE IF EXISTS "Ordenables" CASCADE;
 DROP TABLE IF EXISTS "Usuarios";
+
+-- drop functions
+DROP FUNCTION IF EXISTS insert_into_platillos;
+DROP FUNCTION IF EXISTS update_platillos;
+DROP FUNCTION IF EXISTS insert_into_bebidas;
+DROP FUNCTION IF EXISTS update_bebidas;
 
 --drop types
 DROP TYPE IF EXISTS "OrdenableCategorias";
--- sql/init-db.sh
+++ sql/init-db.sh
@@ -5,4 +5,5 @@ HOST=192.168.1.250
 psql -U $DB_USER -d $DB_NAME -h $HOST -f cleanup.sql
 psql -U $DB_USER -d $DB_NAME -h $HOST -f types.sql
 psql -U $DB_USER -d $DB_NAME -h $HOST -f tables.sql
+psql -U $DB_USER -d $DB_NAME -h $HOST -f views.sql
 psql -U $DB_USER -d $DB_NAME -h $HOST -f seed.sql
