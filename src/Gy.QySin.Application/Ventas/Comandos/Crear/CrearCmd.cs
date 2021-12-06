da4b34a9bf218d7a979da7d195fd9aaad21e82a9
Guayaba Software <guayaba.software@gmail.com>
2021-12-06 13:44:54 -0600

    Comando crear venta

--- src/Gy.QySin.Application/Ventas/Comandos/Crear/CrearCmd.cs
+++ src/Gy.QySin.Application/Ventas/Comandos/Crear/CrearCmd.cs
@@ -6,12 +6,14 @@ using Gy.QySin.Application.Common.Interfaces;
 using MediatR;
 using System;
 using Microsoft.EntityFrameworkCore;
+using Gy.QySin.Domain.ValueObjects;
+using Gy.QySin.Domain.Entities;
 
 namespace Gy.QySin.Application.Ventas.Comandos.Crear
 {
     public class CrearCmd : IRequest
     {
-        public string Nota { get; set; }
+        public string Anotación { get; set; }
         public List<OrdenDto> Bebidas { get; set; }
         public List<OrdenDto> Platillos { get; set; }
     }
@@ -26,16 +28,30 @@ namespace Gy.QySin.Application.Ventas.Comandos.Crear
 
         public async Task<Unit> Handle(CrearCmd request, CancellationToken cancellationToken)
         {
+            var venta = new Venta(request.Anotación);
+
             var bebidas = await repos.Bebidas
                 .AsQueryable()
                 .Where(b => request.Bebidas.Select(x => Guid.Parse(x.Clave))
                     .Contains(b.Clave))
+                .Join(request.Bebidas,
+                    ent => ent.Clave.ToString(),
+                    req => req.Clave,
+                    (ent, req) => new Orden(req.Clave, ent.Nombre, ent.Precio, req.Cantidad))
                 .ToListAsync();
+            venta.AgregarOrdenes(bebidas);
+
             var platillos = await repos.Platillos
                 .AsQueryable()
                 .Where(p => request.Platillos.Select(x => Guid.Parse(x.Clave))
                     .Contains(p.Clave))
+                .Join(request.Platillos,
+                    ent => ent.Clave.ToString(),
+                    req => req.Clave,
+                    (ent, req) => new Orden(req.Clave, ent.Nombre, ent.Precio, req.Cantidad))
                 .ToListAsync();
+            venta.AgregarOrdenes(platillos);
+            // TODO: persistir venta
             throw new System.NotImplementedException();
         }
     }
