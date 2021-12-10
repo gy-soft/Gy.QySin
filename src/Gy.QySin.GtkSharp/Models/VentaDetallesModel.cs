d46e60c72d4b8da41935c5762bf44a99f95c9520
Guayaba Software <guayaba.software@gmail.com>
2021-12-10 14:20:38 -0600

    actualizar cantidad al agregar venta-detalle

--- src/Gy.QySin.GtkSharp/Models/VentaDetallesModel.cs
+++ src/Gy.QySin.GtkSharp/Models/VentaDetallesModel.cs
@@ -16,6 +16,18 @@ namespace Gy.QySin.GtkSharp.Models
 
         public void AgregarVentaDetalle(VentaDetalle ventaDetalle)
         {
+            bool hasAny = GetIterFirst(out TreeIter iter);
+            bool iterUpdated = false;
+            if (hasAny)
+            ApplyToTree(hasAny, ref iter, () =>
+            {
+                if (ventaDetalle.Clave == clave)
+                {
+                    SetValue(iter, 1, cantidad + ventaDetalle.Cantidad);
+                    iterUpdated = true;
+                }
+            });
+            if(!hasAny || !iterUpdated)
             AppendValues(new object[] { 
                 ventaDetalle.Nombre,
                 ventaDetalle.Cantidad,
@@ -25,26 +37,34 @@ namespace Gy.QySin.GtkSharp.Models
         }
         public List<VentaDetalle> ExtraerVentaDetalles()
         {
-            List<VentaDetalle> ventaDetalle = new List<VentaDetalle>();
-            TreeIter iter;
-            if (GetIterFirst(out iter))
+            bool hasAny = GetIterFirst(out TreeIter iter);
+            List<VentaDetalle> ventaDetalles = new List<VentaDetalle>();
+            ApplyToTree(hasAny, ref iter, () =>
             {
-                string nombre;
-                int cantidad;
-                string clave;
-                int categoria;
+                ventaDetalles.Add(
+                    new VentaDetalle(categoria, cantidad, clave, nombre)
+                );
+            });
+            return ventaDetalles;
+        }
+        private void ApplyToTree(bool hasAny, ref TreeIter iter, System.Action action)
+        {
+            if (hasAny)
+            {
                 do
                 {
                     nombre = (string)GetValue(iter, 0);
                     cantidad = (int)GetValue(iter, 1);
                     clave = (string)GetValue(iter, 2);
                     categoria = (int)GetValue(iter, 3);
-                    ventaDetalle.Add(new VentaDetalle(
-                        categoria, cantidad, clave, nombre
-                    ));
+                    action();
                 } while (IterNext(ref iter));
             }
-            return ventaDetalle;
         }
+
+        private string nombre;
+        private int cantidad;
+        private string clave;
+        private int categoria;
     }
 }
