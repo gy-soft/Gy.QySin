862fa6a5c6a1c012027a9fce02842168b902abb2
Guayaba Software <guayaba.software@gmail.com>
2021-12-20 17:52:30 -0600

    inicialización base de datos

--- sql/init-db.sh
+++ sql/init-db.sh
@@ -1,6 +1,6 @@
-DB_USER=QySinDbUser
-DB_NAME=QySinDb
-HOST=192.168.1.250
+DB_USER=qysinuser
+DB_NAME=qysindb
+HOST=127.0.0.1
 
 psql -U $DB_USER -d $DB_NAME -h $HOST -f cleanup.sql
 psql -U $DB_USER -d $DB_NAME -h $HOST -f types.sql
